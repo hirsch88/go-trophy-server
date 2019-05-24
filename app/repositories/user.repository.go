8fdab7cad53a43abaf9f12f242271b417a52d3c2
hirsch <gery.hirschfeld@w3tec.ch>
2019-05-24 21:40:55 +0200

    :loud_sound: Add logs to the user usecase

--- app/repositories/user.repository.go
+++ app/repositories/user.repository.go
@@ -3,17 +3,21 @@ package repositories
 import (
 	"github.com/hirsch88/go-trophy-server/app/models"
 	"github.com/hirsch88/go-trophy-server/app/providers"
+	"go.uber.org/zap"
 )
 
 type userRepository struct {
 	database providers.DatabaseProvider
+	log      *zap.SugaredLogger
 }
 
 func (r *userRepository) Create(user models.User) models.User {
+	r.log.Info("STARTING UserRepository.create()")
 	db := r.database.Connect()
 	defer db.Close()
 
 	db.Create(&user)
+	r.log.Info("FINISHED UserRepository.create()")
 	return user
 }
 
@@ -21,8 +25,8 @@ type UserRepository interface {
 	Create(user models.User) models.User
 }
 
-func NewUserRepository(database providers.DatabaseProvider) UserRepository {
+func NewUserRepository(database providers.DatabaseProvider, log *zap.SugaredLogger) UserRepository {
 	return &userRepository{
-		database,
+		database, log,
 	}
 }
--- app/services/user.service.go
+++ app/services/user.service.go
@@ -5,16 +5,22 @@ import (
 	"github.com/hirsch88/go-trophy-server/app/models"
 	"github.com/hirsch88/go-trophy-server/app/providers"
 	"github.com/hirsch88/go-trophy-server/app/repositories"
+	"go.uber.org/zap"
 )
 
 type userService struct {
 	userRepository repositories.UserRepository
 	mailProvider   providers.MailProvider
+	log *zap.SugaredLogger
 }
 
 func (s *userService) Create(user models.User) models.User {
+	s.log.Info("STARTING UserService.create()")
+
 	newUser := s.userRepository.Create(user)
 	s.mailProvider.Send(mail.NewUserCreatedMail(user), user.Email)
+
+	s.log.Info("FINISHED UserService.create()")
 	return newUser
 }
 
@@ -22,9 +28,10 @@ type UserService interface {
 	Create(user models.User) models.User
 }
 
-func NewUserService(userRepository repositories.UserRepository, mailProvider providers.MailProvider) UserService {
+func NewUserService(userRepository repositories.UserRepository, mailProvider providers.MailProvider, log *zap.SugaredLogger) UserService {
 	return &userService{
 		userRepository,
 		mailProvider,
+		log,
 	}
 }
